bbde22b3d23c33ff59d7cce3eb5cd4468be0f97e
Eduardo Pacheco <eapacheco@users.noreply.github.com>
2017-06-27 18:55:45 -0300

    Correção de bugs

--- query_reporttreinadores.sql
+++ query_reporttreinadores.sql
@@ -1,7 +1,11 @@
+
 select p.preparador_id, p.nome, p.iscpf as brasileiro,
-    count(distinct td.ispositivo) as pegos_doping, -- distinct nao considera valores nulos
-    count(td.ispositivo) as total_atletas, -- sem distinct considera valores nulos
-    (count(distinct td.ispositivo) / count(td.ispositivo)) as razao
+    count(distinct td.atleta_id) as pegos_doping, -- atletas distintos que apareceram nos testes de doping positivos
+    count(distinct a.atleta_id) as total_atletas, -- total de atletas diferentes
+    (case count(distinct a.atleta_id)
+        when 0 then 0
+        else (count(distinct td.atleta_id) / count(distinct a.atleta_id))
+    end) as razao
 from preparador p
     join atleta_participa ap on (p.preparador_id = ap.equipe_id)
     join atleta a on (a.atleta_id = ap.atleta_id)
