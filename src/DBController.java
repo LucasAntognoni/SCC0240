9dfcc667f47e65d6dc786c22ba0aee5dfa2944ab
Lucas Antognoni <antognoni.lucas@gmail.com>
2017-06-27 19:10:29 -0300

    Interface Gráfica

---
+++ src/DBController.java
@@ -0,0 +1,36 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by lucas on 27/06/17.
+ */
+public class DBController {
+
+    @FXML // fx:id = botaoInserir
+    private Button botaoInserir;
+
+    @FXML // fx:id = botaoEditar
+    private Button botaoEditar;
+
+    @FXML // fx:id = botaoDeletar
+    private Button botaoDeletar;
+
+
+    @FXML
+    void handleInserir(ActionEvent event){
+
+    }
+
+    @FXML
+    void handleEditar(ActionEvent event){
+
+    }
+
+    @FXML
+    void handleDeletar(ActionEvent event){
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    private static BorderPane root = new BorderPane();
+
+    public static BorderPane getRoot() {
+        return root;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        URL menuUrl = getClass().getResource("Menu.fxml");
+        MenuBar menu = FXMLLoader.load( menuUrl );
+
+        URL mainPaneUrl = getClass().getResource("MainPane.fxml");
+        AnchorPane mainPane = FXMLLoader.load( mainPaneUrl );
+
+
+        root.setTop(menu);
+        root.setCenter(mainPane);
+
+        Scene scene = new Scene(root, 640, 480);
+
+        /*scene
+                .getStylesheets()
+                .add(getClass()
+                        .getResource("application.css")
+                        .toExternalForm());
+        */
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
